053bddf8db118d2507adaa83c3cf56cccc55288e
Swetha Nethmi <snethmi18@gmail.com>
2025-08-10 20:04:42 +0530

    create save favorites endpoint

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,14 +1,44 @@
-import express from "express"
+import express from "express";
 import { ENV } from "./config/env.js";
+import { db } from "./config/db.js";
+import { favoritesTable } from "./db/schema.js";
 
-
-const app = express()
+const app = express();
 const PORT = ENV.PORT || 5001;
 
-app.get("/api/health",(req,res)=>{
-    res.status(200).json({success:true});
-})
+app.use(express.json());
 
-app.listen(PORT,()=>{
-    console.log("Server is running on PORT:",PORT);
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ success: true });
+});
+
+app.post("/api/favorites", async (req, res) => {
+  try {
+    const { userId, recipeId, title, image, cookTime, servings } = req.body;
+    if (!userId || !recipeId || !title) {
+      return res.status(400).json({ error: "Missing required fields" });
+    }
+
+    const newFavorite = await db
+      .insert(favoritesTable)
+      .values({
+        userId,
+        recipeId,
+        title,
+        image,
+        cookTime,
+        servings,
+      })
+      .returning();
+
+    res.status(201).json(newFavorite[0]);
+  } catch (error) {
+  console.error("Error adding favourite:", error.message, error.stack);
+  res.status(500).json({ error: error.message });
+}
+
+});
+
+app.listen(PORT, () => {
+  console.log("Server is running on PORT:", PORT);
 });
