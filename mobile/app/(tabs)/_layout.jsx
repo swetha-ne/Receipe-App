d81e8dfedcabb8cc676393e6d34ba56f2a6fb8ee
Swetha Nethmi <snethmi18@gmail.com>
2025-08-15 10:10:19 +0530

    feat:make frontend folder structure

---
+++ mobile/app/(tabs)/_layout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { Redirect, Stack } from "expo-router";
+import { useAuth } from "@clerk/clerk-expo";
+
+const TabsLayout = () => {
+  const { isSignedIn } = useAuth();
+
+  if (!isSignedIn) return <Redirect href={"/(auth)/sign-in"} />;
+  return <Stack />;
+};
+
+export default TabsLayout;
